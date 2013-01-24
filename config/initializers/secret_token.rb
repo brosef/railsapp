bf7793c05f1dfb1e199ed0b23fd3c4543b24876e
mmonteleone <mmonteleone@trueblueinc.com>
2013-01-23 22:42:08 -0800

    Adding git-related stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsapp::Application.config.secret_token = '3dd19294bc7ad23b070b08b128c74db2d898fbfc9ce560796fb033c8446726dead0c423b2dd3f7e3054fad9247968de3029dab04d9d826c472fd231627f931d1'
